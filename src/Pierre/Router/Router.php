b477fdbd59ab1921b70f7e8f5ebb1e1b114dcc6d
pierrenoel <pierrenoel1509@gmail.com>
2023-06-19 22:21:16 +0200

    add put and pach routes

--- src/Pierre/Router/Router.php
+++ src/Pierre/Router/Router.php
@@ -57,6 +57,28 @@ class Router
     }
 
     /**
+     * Adds a PUT route.
+     *
+     * @param string $uri The URI of the route.
+     * @param object $callable The callable function of the route.
+     */
+    public function put(string $uri, object $callable)
+    {
+        $this->add('PUT',$uri,$callable);
+    }
+
+    /**
+     * Adds a PATCH route.
+     *
+     * @param string $uri The URI of the route.
+     * @param object $callable The callable function of the route.
+     */
+    public function patch(string $uri, object $callable)
+    {
+        $this->add('PATCH',$uri,$callable);
+    }
+
+    /**
      * Adds a DELETE route.
      *
      * @param string $uri The URI of the route.
